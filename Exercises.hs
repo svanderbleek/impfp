be42845e4afd83accc6ba7b3d8bfab0200abd924
Sandy <sandy.vanderbleek@gmail.com>
2017-06-03 17:39:51 -0400

    second set of exercises

--- Exercises.hs
+++ Exercises.hs
@@ -5,6 +5,7 @@ module Exercises where
 import Control.Monad.State.Lazy
   (execState
   ,evalState
+  ,runState
   ,put
   ,get
   ,forM_
@@ -21,12 +22,20 @@ import Data.Map
 
 import Prelude
   (Int
+  ,Double
   ,(+)
+  ,(-)
+  ,(/)
+  ,(*)
   ,($)
   ,(<$>)
   ,(<*>)
+  ,(>>)
   ,flip
-  ,undefined ,Ord
+  ,fst
+  ,sqrt
+  ,undefined
+  ,Ord
   ,Eq
   ,Show
   ,Maybe(..))
@@ -78,3 +87,104 @@ labelWithCounter (Leaf a) = do
   c <- get
   modify (+1)
   return $ Leaf (a, c)
+
+-- | CalcState
+--
+-- source: http://cmsc-16100.cs.uchicago.edu/2016/Lectures/18-state-monad-1.php
+--
+-- >>> :{
+--  calc $ do
+--   push 1
+--   push 2
+--   add
+--   push 3
+--   mul
+-- :}
+-- 9.0
+
+type CalcState
+  = State [Double]
+
+type Calculation
+  = CalcState ()
+
+pop :: CalcState Double
+pop = do
+  stack <- get
+  case stack of
+    [] -> return 0.0
+    x:xs -> do
+      put xs
+      return x
+
+push :: Double -> CalcState ()
+push d = do
+  stack <- get
+  put (d:stack)
+
+binOp :: (Double -> Double -> Double) -> CalcState ()
+binOp f = do
+  x <- pop
+  y <- pop
+  push $ f x y
+
+add, sub, mul, div :: Calculation
+add =
+  binOp (+)
+sub =
+  binOp (-)
+mul =
+  binOp (*)
+div =
+  binOp (/)
+
+calc :: Calculation -> Double
+calc c =
+  evalState (c >> pop) []
+
+swap :: Calculation
+swap = do
+  x <- pop
+  y <- pop
+  push x
+  push y
+
+dup :: Calculation
+dup = do
+  x <- pop
+  push x
+  push x
+
+unOp :: (Double -> Double) -> CalcState ()
+unOp f = do
+  x <- pop
+  push $ f x
+
+sqrtc :: Calculation
+sqrtc =
+  unOp sqrt
+
+sqrc :: Calculation
+sqrc = do
+  dup
+  mul
+
+-- | store / recall
+--
+-- modify state to have memory cell
+-- store and recall top of stack to this cell
+
+data InternalState
+  = InternalState
+  { stack :: [Double]
+  , memory :: Double }
+
+-- type CalcState = State InternalState
+
+store :: Calculation
+store =
+  undefined
+
+recall :: Calculation
+recall =
+  undefined
